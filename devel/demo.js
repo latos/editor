21212152e97d55e0571750b1dbd1f0534e434843
Ben Lambert-Smith <blambo@gmail.com>
2015-01-13 13:07:35 +1100

    Adding handling of enter keypress with split functions

--- devel/demo.js
+++ devel/demo.js
@@ -3,6 +3,27 @@
 // This script puts "qed" in the global scope.
 require('../src/client');
 
+splitLeftHandler = function(point) {
+
+  point.splitLeft();
+
+  // We want to stop bubbling the handling up once we reach the editor level of the DOM
+  if (point.node.parentNode.id === 'editor') {
+    return true;
+  }
+
+  return false
+}
+
+splitRightHandler = function(point) {
+  if (point.node.parentNode.id === 'editor') {
+    return true;
+  }
+
+  point.splitRight();
+
+  return false
+}
 
 window.onload = function() {
   var Editor = qed.Editor;
@@ -19,14 +40,26 @@ window.onload = function() {
     onKeypress: function(e) {
       // quick hack to test plumbing
       if (e.keyCode == 13) {
-        console.log('enter, saving sel markers');
-        editor.selection().saveToMarkers();
+        e.preventDefault();
       }
+      return true;
     },
     onKey: function(e) {
       if (e.keyType == 'backspace' && e.point.type == 'start') {
         e.preventDefault();
+      } else if (e.keyCode == 13) {
+        result = splitLeftHandler(e.point);
+        showContent();
+
+        // We set the caret to the split point
+        editor.selection().setCaret(e.point);
+
+        return result;
       }
+      return true;
+    },
+    // Trying to get rid of error messaging for unhandled events to clear up debugging messages
+    onSelection: function(e) {
       return true;
     }
   });
