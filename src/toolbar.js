d303e9ddfc3434a0c2f93c5b172d6d7a102b7a7e
Ben Lambert-Smith <blambo@gmail.com>
2015-02-06 16:00:14 +1100

    Adding addButton to toolbar.js

--- src/toolbar.js
+++ src/toolbar.js
@@ -2,11 +2,15 @@ var util = require('./util');
 var Editor = require('./event-bus');
 var Selection = require('./selection');
 
+module.exports = Toolbar;
+
 /**
  * Default Toolbar
  */
-module.exports = function Toolbar(editor) {
+function Toolbar(editor) {
   var me = this;
+
+  me.editor = editor;
 
   editor.addListener({
     onSelection: function(selection) {
@@ -40,11 +44,26 @@ module.exports = function Toolbar(editor) {
   me.elem.style.background = 'white';
   me.elem.style.boxShadow = '0px 3px 15px rgba(0,0,0,0.2)';
 
-
-  me.elem.innerText = 'sup toolbar';
-
   document.body.appendChild(me.elem);
-}
+};
 
+/*
+ * Add a button to the toolbar
+ * label:    The text to show as a button label
+ * check:    A function that returns true if text nodes in current selection are wrapped in style
+ * callback: A function that takes the editor and result of check, adds style to current selection, or removes the style if check return false
+ */
+Toolbar.prototype.addButton = function(label, check, callback) {
+  var me = this;
 
+  var newButton = document.createElement("button");
+  buttonLabel = document.createTextNode(label);
+  newButton.appendChild(buttonLabel);
+
+  newButton.onclick=function(){
+    callback(me.editor, check(me.editor));
+  };
+
+  me.elem.appendChild(newButton);
+};
 
