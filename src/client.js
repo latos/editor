744beb7c97d880aca6b5f7071a0b0f72c533f05f
Daniel Danilatos <daniel@helixta.com.au>
2015-02-12 14:32:24 +1100

    inline decorator range attribute parsing

--- src/client.js
+++ src/client.js
@@ -3,7 +3,9 @@ window.qed = {
   Editor: require('./editor'),
   Point: require('./point'),
   Range: require('./range'),
-  Toolbar: require('./toolbar')
+  Toolbar: require('./toolbar'),
+
+  InlineDecorator: require('./inline-decorator')
 
   // TODO: Just export all the classes so people can put them together
   //       however they like.
---
+++ src/inline-decorator.spec.js
@@ -0,0 +1,76 @@
+var _ = require('lodash');
+var util = require('./util');
+var assert = util.assert;
+var InlineDecorator = require('./inline-decorator');
+var tutil = require('./test-util');
+var Point = require('./point');
+var Q = require('q');
+
+describe('InlineDecorator', function() {
+  var testCases = [
+    ['|', {}],
+    ['|a', {}],
+    ['a|', {}],
+    ['[a]', {}],
+
+    ['<b>|</b>', {'font-weight':['bold']}],
+    ['<b>|a</b>', {'font-weight':['bold']}],
+    ['<b>a|</b>', {'font-weight':['bold']}],
+    ['<b>[a]</b>', {'font-weight':['bold']}],
+
+    ['<p>a|bc<i>sup</i></p><p>hello<b>sup</b></p>', {}],
+    ['<p>abc<i>|sup</i></p><p>hello<b>sup</b></p>', {'font-style':['italic']}],
+    ['<p>abc<i>sup</i></p><p>hello<b>|sup</b></p>', {'font-weight':['bold']}],
+    ['<p>ab[c<i>s]up</i></p><p>hello<b>sup</b></p>', {'font-style':['italic','normal']}],
+    ['<p>|</p>', {}]
+
+  ];
+
+  it('getRangeAttributes', tutil.promised(function() {
+    return tutil.rangeCases(testCases, function(elem, range, expected) {
+
+      // So decorator thinks it's in an editor
+      elem.contentEditable = 'true';
+
+      assert(elem.$wnd);
+
+      var idec = new InlineDecorator(elem.$wnd);
+
+      var fullExpected = expandWithDefaults(expected, idec.getDefaults());
+      var attrs = idec.getRangeAttributes(range);
+
+      sortArrays(attrs);
+      sortArrays(fullExpected);
+
+      expect(attrs).toEqual(fullExpected);
+    }, function wrap(testContent) {
+      return '<div contentEditable="true">' + testContent + '</div>';
+    });
+  }));
+
+});
+
+function sortArrays(mapOfArrays) {
+  for (var k in mapOfArrays) {
+    mapOfArrays[k].sort();
+  }
+}
+
+/**
+ * Expands the given attrs to encompass the full set of keys with at least
+ * one value for each key, given the defaults.
+ */
+function expandWithDefaults(attrs, defaults) {
+
+  var filled = _.assign({}, attrs);
+  for (var k in defaults) {
+    if (!filled[k]) {
+      filled[k] = [];
+    }
+    if (filled[k].length === 0) {
+      filled[k].push(defaults[k]);
+    }
+  }
+
+  return filled;
+}
