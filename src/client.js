6feefc305ae347bc2f1209a10b75b3593a979f9d
Ben Lambert-Smith <blambo@gmail.com>
2015-02-12 17:23:10 +1100

    Exposing util for use in implementation

--- src/client.js
+++ src/client.js
@@ -3,7 +3,8 @@ window.qed = {
   Editor: require('./editor'),
   Point: require('./point'),
   Range: require('./range'),
-  Toolbar: require('./toolbar')
+  Toolbar: require('./toolbar'),
+  util: require('./util')
 
   // TODO: Just export all the classes so people can put them together
   //       however they like.
