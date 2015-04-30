572621d62fdab490c84a3902a6c9f7fa3a17f5b0
Dylan Baskind <dylan.baskind@gmail.com>
2015-04-30 14:58:43 +1000

    Fixed Toolbar positioning

--- src/util.js
+++ src/util.js
@@ -284,3 +284,20 @@ exports.map = function(arr, func) {
   }
   return result;
 };
+
+
+// From: http://davidwalsh.name/javascript-debounce-function
+exports.debounce = function (func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
