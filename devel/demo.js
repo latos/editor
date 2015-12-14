e18e145b0f5194310f6e3d06c9802eb5eb308371
Ben Lambert-Smith <blambo@gmail.com>
2015-12-14 13:39:08 +1100

    Cleaning up demo.js to remove unused code

--- devel/demo.js
+++ devel/demo.js
@@ -2,55 +2,6 @@
 // In a non-commonjs app, would include this with a script tag.
 // This script puts "qed" in the global scope.
 require('../src/client');
-
-splitLeftHandler = function(point) {
-
-  point.splitLeft();
-
-  // We want to stop bubbling the handling up once we reach the editor level of the DOM
-  if (point.node.parentNode.id === 'editor') {
-    return true;
-  }
-
-  return false
-}
-
-splitRightHandler = function(point) {
-  if (point.node.parentNode.id === 'editor') {
-    return true;
-  }
-
-  point.splitRight();
-
-  return false
-}
-
-joinLeftHandler = function(point) {
-  var result;
-  if (point.node.parentNode.id === 'editor') {
-    result = true;
-  }
-  else {
-    result = false;
-  }
-  point.joinLeft();
-
-  return result;
-};
-
-joinRightHandler = function(point) {
-  point.joinRight();
-
-  if (point.node.parentNode.id === 'editor') {
-    return true;
-  }
-  else {
-    return false;
-  }
-};
-
-
-
 // -----------------------------------------------------
 
 window.onload = function() {
@@ -70,24 +21,6 @@ window.onload = function() {
       return true;
     },
     onKey: function(e) {
-      // if (e.keyType == 'backspace' && e.point.type == 'start') {
-      //   e.preventDefault();
-      //   //result = joinRightHandler(e.point);
-      //   result = joinLeftHandler(e.point);
-      //   showContent();
-
-      //   editor.selection().setCaret(e.point);
-
-      //   return result;
-      // } else if (e.keyCode == 13) {
-      //   result = splitRightHandler(e.point);
-      //   showContent();
-
-      //   // We set the caret to the split point
-      //   editor.selection().setCaret(e.point);
-
-      //   return result;
-      // }
       return false;
     },
   });
@@ -95,15 +28,6 @@ window.onload = function() {
   editor.attach($('editor'));
   editor.focus();
   showContent();
-
-  var tracker = new StemTracker(editor, function(){
-    /* Click handler goes here */
-  });
-  var toolbar = new Toolbar(editor);
-
-  var placeholders = new Placeholders(editor);
-  placeholders.addPlaceholder('P', "lol text");
-  placeholders.startTracking();
 
   function showContent() {
     $('content').innerHTML = '';
